925d43f5edb90f239c89e123795a4805c5797066
Shubh Shetgaonkar <shetgaonkarshubh@gmail.com>
2026-09-17 12:47:51 +0530

    Fix SHA misk work

--- app.js
+++ app.js
@@ -1507,3 +1507,7 @@ document.addEventListener('DOMContentLoaded', () => {
         pushBtn.addEventListener('click', explicitPush);
     }
 });
+
+document.getElementById('open-sync-modal-btn')?.addEventListener('click', () => {
+    document.getElementById('sync-modal').classList.remove('hidden');
+});
